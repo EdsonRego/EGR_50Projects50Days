78739527507ee0ddcfec9681bc6648b5ee183533
Edson Gomes do Rego <edsonxrego@gmail.com>
2026-06-02 20:06:42 -0300

    Day 11 event keycodes

---
+++ day11_event-keycodes/script.js
@@ -0,0 +1,25 @@
+const insert = document.getElementById('insert')
+const restartButton = document.getElementById('restart')
+
+window.addEventListener('keydown', (event) => {
+  insert.innerHTML = `
+  <div class="key">
+  ${event.key === ' ' ? 'Space' : event.key} 
+  <small>event.key</small>
+</div>
+
+<div class="key">
+  ${event.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${event.code}
+  <small>event.code</small>
+</div>
+  `
+})
+
+restartButton.addEventListener('click', () => {
+  window.location.reload()
+})
